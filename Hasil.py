213676642199c34157e4543b63579966d4dac834
AidenMoon1 <121096430+AidenMoon1@users.noreply.github.com>
2024-02-29 11:21:25 +0700

    Add files via upload

---
+++ Hasil.py
@@ -0,0 +1,10 @@
+#sarimin pergi ke pasar
+rokok = 20000
+minyak = 25000
+gula = 11000
+saldo = 100000
+diskon = 10000
+
+#hitung hasil
+total_belanjaan= saldo - (rokok + minyak + gula - diskon)
+print(total_belanjaan)
